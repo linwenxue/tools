ff56936f7adb0b0daded44458fac5c251de1009f
linwenxue <xiaolinziemail@163.com>
2017-01-16 15:48:46 +0800

    1.add hive udf test

--- src/main/scala/com/lin/study/spark/streaming/Test.scala
+++ src/main/scala/com/lin/study/spark/streaming/Test.scala
@@ -12,21 +12,21 @@ object Test {
       case "aaa" | "bbb" => println("match ..")
       case _ => println("not match")
     }
-    val str = "21,12,32,45,67,1,4,5,78".split(",").map { x =>
+    val str = "21,12".split(",").map { x =>
       x.toInt
     }.toList.sortWith(_ < _).slice(0,3)
     str.foreach(println)
 
-    val dt1 = DateTime.parse("2016111110", DateTimeFormat.forPattern("yyyyMMddHH"))
-    val dt2 = DateTime.parse("2016120110", DateTimeFormat.forPattern("yyyyMMddHH"))
+    val dt1 = DateTime.parse("2017010415", DateTimeFormat.forPattern("yyyyMMddHH"))
+    val dt2 = DateTime.parse("2017010115", DateTimeFormat.forPattern("yyyyMMddHH"))
     println("时间相差：")
     println(Days.daysBetween(dt1, dt2).getDays() + " 天 ")
     println(Hours.hoursBetween(dt1, dt2).getHours() + " 小时 ")
     println(Minutes.minutesBetween(dt1, dt2).getMinutes() + " 分钟 ")
     println(Seconds.secondsBetween(dt1, dt2).getSeconds()+ " 秒")
 
+    val str2 = "23,45,34,36".split(",").toList.slice(0,6)
+    str2.foreach(println)
+    println(str2.length)
   }
-
-
-
 }
