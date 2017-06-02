cb4ddd64b499bac7fc73d9d44d55e55e2bad18b4
linwenxue <xiaolinziemail@163.com>
2017-06-02 14:19:11 +0800

    1.添加测试验证reflect代码

---
+++ src/main/java/com/lin/study/java/reflect/FooExtend.java
@@ -0,0 +1,16 @@
+package com.lin.study.java.reflect;
+
+/**
+ * Created by wenxuelin on 2017/6/2.
+ */
+public class FooExtend extends Foo {
+    private String desc;
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
