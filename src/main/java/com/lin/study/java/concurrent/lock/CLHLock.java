05c861f7a2f575d1fcd92079c0cb996812277934
linwenxue <xiaolinziemail@163.com>
2017-07-04 14:48:45 +0800

    1.add SPIN CLH MCS'lock code

---
+++ src/main/java/com/lin/study/java/concurrent/lock/CLHLock.java
@@ -0,0 +1,35 @@
+package com.lin.study.java.concurrent.lock;
+
+import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
+
+/**
+ * CLH锁也是一种基于链表的可扩展、高性能、公平的自旋锁，
+ * 申请线程只在本地变量上自旋，它不断轮询前驱的状态，如果发现前驱释放了锁就结束自旋。
+ * Created by wenxuelin on 2017/7/4.
+ */
+public class CLHLock {
+    public static class CLHNode {
+        private boolean isLocked = true; // 默认是在等待锁
+    }
+
+    @SuppressWarnings("unused" )
+    private volatile CLHNode tail;
+    private static final AtomicReferenceFieldUpdater<CLHLock, CLHNode> UPDATER = AtomicReferenceFieldUpdater
+            .newUpdater(CLHLock.class, CLHNode.class,"tail");
+
+    public void lock(CLHNode currentThreadCLHNode) {
+        CLHNode preNode = UPDATER.getAndSet(this, currentThreadCLHNode); //把this里的"tail" 值设置成currentThreadCLHNode
+        if(preNode != null) {//已有线程占用了锁，进入自旋
+            while(preNode.isLocked) {
+            }
+        }
+    }
+
+    public void unlock(CLHNode currentThreadCLHNode) {
+        // 如果队列里只有当前线程，则释放对当前线程的引用（for GC）
+        if (!UPDATER.compareAndSet(this, currentThreadCLHNode, null)) {
+            // 还有后续线程
+            currentThreadCLHNode.isLocked = false;//改变状态，让后续线程结束自旋
+        }
+    }
+}
---
+++ src/main/java/com/lin/study/java/concurrent/lock/SpinLock.java
@@ -0,0 +1,30 @@
+package com.lin.study.java.concurrent.lock;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * 自旋锁是指当一个线程尝试获取某个锁时，如果该锁已被其他线程占用，
+ * 就一直循环检测锁是否被释放，而不是进入线程挂起或睡眠状态。
+ * 自旋锁适用于锁保护的临界区很小的情况，临界区很小的话，锁占用的时间就很短。
+ * 缺点：
+ * 1. CAS操作需要硬件的配合；
+ * 2. 保证各个CPU的缓存（L1、L2、L3、跨CPU Socket、主存）的数据一致性，通讯开销很大，在多处理器系统上更严重；
+ * 3. 没法保证公平性，不保证等待进程/线程按照FIFO顺序获得锁。
+ * Created by wenxuelin on 2017/7/4.
+ */
+public class SpinLock {
+    private AtomicReference<Thread> owner = new AtomicReference<Thread>();
+
+    public void lock() {
+        Thread currentThread = Thread.currentThread();
+        // 如果锁未被占用，则设置当前线程为锁的拥有者
+        while(owner.compareAndSet(null, currentThread)) {
+        }
+    }
+
+    public void unlock() {
+        Thread currentThread = Thread.currentThread();
+        // 只有锁的拥有者才能释放锁
+        owner.compareAndSet(currentThread, null);
+    }
+}
