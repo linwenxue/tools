246bb998d9bb806528d3b6a3f8a6fcec8ab3d596
linwenxue <xiaolinziemail@163.com>
2017-07-04 15:52:14 +0800

    1.add SPIN CLH MCS'lock code

--- src/main/java/com/lin/study/java/concurrent/lock/TicketLock.java
+++ src/main/java/com/lin/study/java/concurrent/lock/TicketLock.java
@@ -11,8 +11,7 @@ import java.util.concurrent.atomic.AtomicInteger;
  * 缺点
  * Ticket Lock 虽然解决了公平性的问题，但是多处理器系统上，
  * 每个进程/线程占用的处理器都在读写同一个变量serviceNum ，
- * 每次读写操作都必须在多个处理器缓存之间进行缓存同步，这会导致繁重的系统总线和内存的流量，
- * 大大降低系统整体的性能。
+ * 每次读写操作都必须在多个处理器缓存之间进行缓存同步，这会导致繁重的系统总线和内存的流量，大大降低系统整体的性能。
  * Created by wenxuelin on 2017/7/4.
  */
 public class TicketLock {
