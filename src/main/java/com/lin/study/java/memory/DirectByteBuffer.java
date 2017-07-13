aaedb4753a72f9786c55e2a9e55480cb4602af10
linwenxue <xiaolinziemail@163.com>
2017-07-13 14:19:31 +0800

    1.add test code

---
+++ src/main/java/com/lin/study/java/memory/DirectByteBuffer.java
@@ -0,0 +1,22 @@
+package com.lin.study.java.memory;
+
+import sun.nio.ch.DirectBuffer;
+
+import java.nio.ByteBuffer;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 分配堆外内存，系统内存及释放内存
+ * Created by wenxuelin on 2017/7/13.
+ */
+public class DirectByteBuffer {
+    public static void main(String[] args) throws Throwable{
+        //分配512MB直接缓存
+        ByteBuffer bb = ByteBuffer.allocateDirect(1024*1024*512);
+        TimeUnit.SECONDS.sleep(10);
+        //清除直接缓存
+        ((DirectBuffer)bb).cleaner().clean();
+        TimeUnit.SECONDS.sleep(10);
+        System.out.println("ok");
+    }
+}
--- src/main/java/com/lin/study/java/thread/concurrent/ExecutorServiceTest.java
+++ src/main/java/com/lin/study/java/thread/concurrent/ExecutorServiceTest.java
@@ -9,6 +9,10 @@ import java.util.concurrent.Executors;
 public class ExecutorServiceTest {
     public static void main(String[] args) {
         ExecutorService executorService = Executors.newCachedThreadPool();
-        executorService.execute(null);
+        executorService.execute(new Runnable() {
+            public void run() {
+                System.out.println("test");
+            }
+        });
     }
 }
