8d0eca04e4209d5d892bde00f2d9ae3f62307fd5
linwenxue <xiaolinziemail@163.com>
2017-09-07 15:05:44 +0800

    1.update leetcode algorithm code

--- src/main/java/com/lin/algorithm/LongestSubstring.java
+++ src/main/java/com/lin/algorithm/LongestSubstring.java
@@ -22,27 +22,24 @@ public class LongestSubstring {
     /**
      * The naive approach is very straightforward. But it is too slow. So how can we optimize it?
      *
-     * In the naive approaches, we repeatedly check a substring to see if it has duplicate character. But it is unnecessary. If a substring s_{ij}s
-     * ij
-     * from index ii to j - 1j−1 is already checked to have no duplicate characters. We only need to check if s[j]s[j] is already in the substring s_{ij}s
-     * ij
+     * In the naive approaches, we repeatedly check a substring to see if it has duplicate character. But it is unnecessary. If a substring s_{ij}
+     * from index ii to j - 1 is already checked to have no duplicate characters. We only need to check if s[j] is already in the substring s_{ij}
      * .
      *
-     * To check if a character is already in the substring, we can scan the substring, which leads to an O(n^2)O(n
-     * 2
-     * ) algorithm. But we can do better.
+     * To check if a character is already in the substring, we can scan the substring, which leads to an O(n^2)
+     * algorithm. But we can do better.
      *
      * By using HashSet as a sliding window, checking if a character in the current can be done in O(1)O(1).
      *
-     * A sliding window is an abstract concept commonly used in array/string problems. A window is a range of elements in the array/string which usually defined by the start and end indices, i.e. [i, j)[i,j) (left-closed, right-open). A sliding window is a window "slides" its two boundaries to the certain direction. For example, if we slide [i, j)[i,j) to the right by 11 element, then it becomes [i+1, j+1)[i+1,j+1) (left-closed, right-open).
+     * A sliding window is an abstract concept commonly used in array/string problems. A window is a range of elements in the array/string which usually defined by the start and end indices, i.e. [i, j) (left-closed, right-open). A sliding window is a window "slides" its two boundaries to the certain direction. For example, if we slide [i, j) to the right by 1 element, then it becomes [i+1, j+1) (left-closed, right-open).
      *
-     * Back to our problem. We use HashSet to store the characters in current window [i, j)[i,j) (j = ij=i initially). Then we slide the index jj to the right. If it is not in the HashSet, we slide jj further. Doing so until s[j] is already in the HashSet. At this point, we found the maximum size of substrings without duplicate characters start with index ii. If we do this for all ii, we get our answer.
+     * Back to our problem. We use HashSet to store the characters in current window [i, j) (j=i initially). Then we slide the index j to the right. If it is not in the HashSet, we slide j further. Doing so until s[j] is already in the HashSet. At this point, we found the maximum size of substrings without duplicate characters start with index i. If we do this for all i, we get our answer.
 
      * Complexity Analysis
 
-     * Time complexity : O(2n) = O(n)O(2n)=O(n). In the worst case each character will be visited twice by ii and jj.
+     * Time complexity : O(2n)=O(n). In the worst case each character will be visited twice by i and j.
 
-     * Space complexity : O(min(m, n))O(min(m,n)). Same as the previous approach. We need O(k)O(k) space for the sliding window, where kk is the size of the Set. The size of the Set is upper bounded by the size of the string nn and the size of the charset/alphabet mm.
+     * Space complexity : O(min(m, n)). Same as the previous approach. We need O(k) space for the sliding window, where k is the size of the Set. The size of the Set is upper bounded by the size of the string nn and the size of the charset/alphabet mm.
      * @param s
      * @return
      */
@@ -68,13 +65,10 @@ public class LongestSubstring {
      * Sliding Window Optimized [Accepted]
      * The above solution requires at most 2n steps. In fact, it could be optimized to require only n steps. Instead of using a set to tell if a character exists or not, we could define a mapping of the characters to its index. Then we can skip the characters immediately when we found a repeated character.
      *
-     * The reason is that if s[j]s[j] have a duplicate in the range [i, j)[i,j) with index j'j
-     * ′
-     * , we don't need to increase ii little by little. We can skip all the elements in the range [i, j'][i,j
-     * ′
-     * ] and let ii to be j' + 1j
-     * ′
-     * +1 directly.
+     * The reason is that if s[j] have a duplicate in the range [i, j) with index j,
+     * we don't need to increase i little by little. We can skip all the elements in the range [i, j]
+     * and let i to be j + 1 directly.
+     *
      * @param s
      * @return
      */
@@ -108,11 +102,11 @@ public class LongestSubstring {
      *
      * Complexity Analysis
      *
-     * Time complexity : O(n)O(n). Index jj will iterate nn times.
+     * Time complexity : O(n). Index j will iterate n times.
      *
-     * Space complexity (HashMap) : O(min(m, n))O(min(m,n)). Same as the previous approach.
+     * Space complexity (HashMap) : O(min(m, n)). Same as the previous approach.
      *
-     * Space complexity (Table): O(m)O(m). mm is the size of the charset.
+     * Space complexity (Table): O(m). m is the size of the charset.
      * @param s
      * @return
      */
@@ -126,5 +120,9 @@ public class LongestSubstring {
             index[s.charAt(j)] = j + 1;
         }
         return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new LongestSubstring().lengthOfLongestSubstring3("abcdeeeaf"));
     }
 }
