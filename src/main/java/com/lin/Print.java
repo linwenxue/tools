e4d83d374b0681664b294912185d77b80e7c0a5e
linwenxue <xiaolinziemail@163.com>
2017-09-25 18:48:58 +0800

    1.add tool print class

---
+++ src/main/java/com/lin/Print.java
@@ -0,0 +1,14 @@
+package com.lin;
+
+public class Print {
+
+    public static void print(Object o) {
+        System.out.print(o);
+    }
+
+    public static void println(Object o) {
+        System.out.println(o);
+    }
+
+
+}
