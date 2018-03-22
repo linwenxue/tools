fc2dd243e38c2b73f735522fa354f4f59522f006
linwenxue <xiaolinziemail@163.com>
2018-03-22 15:29:35 +0800

    1.commit quicksort code

--- src/main/java/com/lin/java/sort/QuickSort.java
+++ src/main/java/com/lin/java/sort/QuickSort.java
@@ -3,9 +3,14 @@ package com.lin.java.sort;
 import java.util.Arrays;
 
 /**
- * 快速排序：
+ * 快速排序基本思想：
+ * 通过一趟排序将要排序的数据分割成独立的两部分，
+ * 其中一部分的所有数据都比另外一部分的所有数据都要小，
+ * 然后再按此方法对这两部分数据分别进行快速排序，
+ * 整个排序过程可以递归进行，以此达到整个数据变成有序序列。
  */
 public class QuickSort {
+
     public void quicksort(int n[], int left, int right) {
         int dp;
         if (left < right) {
@@ -15,14 +20,15 @@ public class QuickSort {
         }
     }
 
+
     public int partition(int n[], int left, int right) {
         int pivot = n[left];
         while (left < right) {
-            while (left < right && n[right] >= pivot)
+            while (left < right && n[right] > pivot)
                 right--;
             if (left < right)
                 n[left++] = n[right];
-            while (left < right && n[left] <= pivot)
+            while (left < right && n[left] < pivot)
                 left++;
             if (left < right)
                 n[right--] = n[left];
@@ -32,10 +38,24 @@ public class QuickSort {
     }
 
 
+    /*public int partition2(int[] n, int start, int end) {
+        int flag = n[start];
+        while (start < end) {
+            while(start < end && n[end] > flag) end--;
+            n[start++] = n[end];
+            while(start < end && n[start] < flag) start++;
+            n[end--] = n[start];
+        }
+        n[start] = flag;
+        return start;
+    }*/
+
+
     public static void main(String[] args) {
         QuickSort sorter = new QuickSort();
         int[] a = {49,38,65,97,76,13,27,49,78,34,12,64,5,4,62,99,98,54,56,17,18,23,34,15,35,25,53,51};
         sorter.quicksort(a, 0, a.length - 1);
         Arrays.stream(a).boxed().map(x -> String.valueOf(x) + " ").forEach(System.out::print);
+        System.out.println();
     }
 }
--- src/main/java/com/lin/java/sort/ThreeColorSort.java
+++ src/main/java/com/lin/java/sort/ThreeColorSort.java
@@ -6,6 +6,13 @@ import java.util.Random;
 /**
  * 输入一个整型数组，每个元素在0~2之间，其中0，1，2分别代表红、白、蓝。
  * 现要求对数组进行排序，相同颜色的在一起，而且按红白蓝顺序先后排列。要求时间复杂度为O(n)。
+ *
+ * 这个问题类似快排中partition过程，只是需要用到三个指针：一个前指针begin，一个中指针current，一个后指针end，
+ * current指针遍历整个数组序列：
+ * current指针所指元素为0时，与begin指针所指的元素交换，而后current++，begin++ ；
+ * current指针所指元素为1时，不做任何交换（即球不动），而后current++ ；
+ * current指针所指元素为2时，与end指针所指的元素交换，而后，current指针不动，end-- 。
+ 为什么上述第3点中，current指针所指元素为2时，与end指针所指元素交换之后，current指针不能动呢？因为第三步中current指针所指元素与end指针所指元素交换之前，如果end指针之前指的元素是0，那么与current指针所指元素交换之后，current指针此刻所指的元素是0，此时，current指针能动么？不能动，因为如上述第1点所述，如果current指针所指的元素是0，还得与begin指针所指的元素交换。
  */
 public class ThreeColorSort {
     public Integer[] sort(Integer[] array) {
